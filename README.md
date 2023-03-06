879927664dd24a8565419ca562d3d1b1cbf9ca78
Minh Duy Nguyen <nguy0976@algonquinlive.com>
2023-03-06 15:52:09 -0500

    Completes iteration 2

--- README.md
+++
@@ -1,3 +0,0 @@
-# mtm6404-task-manager-nguy0976
-
-Iteration - 1
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+ export default function Navbar() {
+  return (
+    <nav className="nav-container"> 
+      <img src= "images/earth.png" className="nav-icon" alt="earth icon" />
+        <h1 className="nav-title">
+          my travel journal.
+        </h1>      
+         </nav>
+  );
+      
+}
